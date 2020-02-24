f9f744bdea721905288634784d85a50e104f224f
Alexander Ignatov <alexsyscom@yandex.ru>
2020-02-24 12:52:47 +0300

    add smooth scroling

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let hamburger = document.querySelector('.hamburger');
+document.addEventListener('DOMContentLoaded',() =>{
+    let hamburger = document.querySelector('.hamburger');
+    // 
+    hamburger.addEventListener('click', switchmenu);
 
-hamburger.addEventListener('click', switchmenu);
+    function switchmenu(e) {
+        var menu = document.querySelector('#menu');
+        this.open = !this.open;
+        if (this.open) {
+            e.target.className = menu.className = "open";
+        }
+        else {
+                e.target.className = menu.className = "close";
+        }
+    }
+});
 
-function switchmenu(e) {
-    var menu = document.querySelector('#menu');
-    this.open = !this.open;
-    if (this.open) {
-        e.target.className = menu.className = "open";
-    }
-    else {
-    		e.target.className = menu.className = "close";
-    }
-}
+
